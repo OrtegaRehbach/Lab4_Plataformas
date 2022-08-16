40eaac0c24c7a570574cc6f381def103a30e09ee
Alejandro Ortega <ort18248@uvg.edu.gt>
2022-08-16 15:58:45 -0600

    Subida de archivos

---
+++ BasicLayout/Happy Birthday/app/src/main/java/com/uvg/happy_birthday/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uvg.happy_birthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
